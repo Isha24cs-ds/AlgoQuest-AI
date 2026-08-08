23daa54c88a74a044eb46d2eb617c13551996f5e
Isha <2k24.csds1b.2410587@gmail.com>
2026-08-08 22:33:16 +0530

    Developed join/create arena room page

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,41 +1,38 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import StringsQuiz from "./pages/DSA/StringsQuiz";
+import { Routes, Route } from "react-router-dom";
+
+// Main Pages
 import LandingPage from "./pages/LandingPage";
 import JourneySelection from "./pages/JourneySelection";
 import WorldMap from "./pages/WorldMap";
 import GamePortal from "./pages/GamePortal";
+
+// Arena Pages
+import ArenaHome from "./pages/Arena/ArenaHome";
 import RoomSelection from "./pages/Arena/RoomSelection";
 import CreateRoom from "./pages/Arena/CreateRoom";
 import JoinRoom from "./pages/Arena/JoinRoom";
 import WaitingLobby from "./pages/Arena/WaitingLobby";
-import DSAHome from "./pages/DSA/DSAHome";
 
-// Arrays
+// DSA Pages
+import DSAHome from "./pages/DSA/DSAHome";
 import ArrayKingdom from "./pages/DSA/ArrayKingdom";
 import LearnPage from "./pages/DSA/LearnPage";
 import Lesson from "./pages/DSA/Lesson";
 import PracticePage from "./pages/DSA/PracticePage";
 import Question from "./pages/DSA/Question";
 import Quiz from "./pages/DSA/Quiz";
-import ArenaHome from "./pages/Arena/ArenaHome";
-// Strings
+
+// String Pages
 import StringsKingdom from "./pages/DSA/StringsKingdom";
 import LearnStrings from "./pages/DSA/LearnStrings";
 import StringLesson from "./pages/DSA/StringLesson";
+import StringsQuiz from "./pages/DSA/StringsQuiz";
 
 export default function App() {
   return (
     
       <Routes>
-        <Route path="/arena/room" element={<RoomSelection/>}/>
 
-<Route path="/arena/create" element={<CreateRoom/>}/>
-
-<Route path="/arena/join" element={<JoinRoom/>}/>
-
-<Route path="/arena/lobby" element={<WaitingLobby/>}/>
-<Route path="/arena" element={<ArenaHome />}/>
-        <Route path="/strings-quiz" element={<StringsQuiz />} />
         {/* Landing */}
         <Route path="/" element={<LandingPage />} />
 
@@ -48,63 +45,31 @@ export default function App() {
         {/* Game Portal */}
         <Route path="/portal" element={<GamePortal />} />
 
-        {/* DSA Home */}
+        {/* ===================== ARENA ===================== */}
+        <Route path="/arena" element={<ArenaHome />} />
+        <Route path="/arena/room" element={<RoomSelection />} />
+        <Route path="/arena/create" element={<CreateRoom />} />
+        <Route path="/arena/join" element={<JoinRoom />} />
+        <Route path="/arena/lobby" element={<WaitingLobby />} />
+
+        {/* ===================== DSA ===================== */}
         <Route path="/dsa" element={<DSAHome />} />
 
-        {/* ===================== ARRAYS ===================== */}
-
+        {/* Arrays */}
         <Route path="/arrays" element={<ArrayKingdom />} />
-
         <Route path="/learn-arrays" element={<LearnPage />} />
-
-        <Route
-          path="/learn-arrays/:id"
-          element={<Lesson />}
-        />
-
-        <Route
-          path="/practice"
-          element={<PracticePage />}
-        />
-
-        <Route
-          path="/question/:slug"
-          element={<Question />}
-        />
-
-        <Route
-          path="/quiz"
-          element={<Quiz />}
-        />
+        <Route path="/learn-arrays/:id" element={<Lesson />} />
+        <Route path="/practice" element={<PracticePage />} />
+        <Route path="/question/:slug" element={<Question />} />
+        <Route path="/quiz" element={<Quiz />} />
 
         {/* ===================== STRINGS ===================== */}
-
-        <Route
-          path="/strings"
-          element={<StringsKingdom />}
-        />
-
-        <Route
-          path="/learn-strings"
-          element={<LearnStrings />}
-        />
-
-        <Route
-          path="/learn-strings/:id"
-          element={<StringLesson />}
-        />
-
-        {/* Future Routes */}
-
-        {/* <Route path="/strings-practice" element={<StringsPractice />} /> */}
-
-        {/* <Route path="/strings-quiz" element={<StringsQuiz />} /> */}
-
-        {/* <Route path="/strings-mini-boss" element={<StringsMiniBoss />} /> */}
-
-        {/* <Route path="/strings-final-boss" element={<StringsFinalBoss />} /> */}
+        <Route path="/strings" element={<StringsKingdom />} />
+        <Route path="/learn-strings" element={<LearnStrings />} />
+        <Route path="/learn-strings/:id" element={<StringLesson />} />
+        <Route path="/strings-quiz" element={<StringsQuiz />} />
 
       </Routes>
-  
+   
   );
 }
