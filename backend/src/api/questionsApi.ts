b033f572b55e6156608031f769c6e71e1c2b8473
Isha <2k24.csds1b.2410587@gmail.com>
2026-09-01 12:09:04 +0530

    Database deployement

--- backend/src/api/questionsApi.ts
+++ backend/src/api/questionsApi.ts
@@ -1,5 +1,4 @@
-const BASE_URL =
-  import.meta.env.VITE_API_URL || "http://localhost:5000/api/v1";
+const BASE_URL = "http://localhost:5000/api/v1";
 
 export async function getQuestions() {
   const response = await fetch(`${BASE_URL}/questions`);
